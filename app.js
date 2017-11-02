c7888157e60a8996dc44ada8a0d71a6d08959fb6
titay2 <tehetenamas@gmail.com>
2017-11-02 09:12:44 +0200

    data table and reallo diagram

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const myApp = angular.module('myApp', ['ngRoute', 'ui.bootstrap'])
 const market = JSON.parse(localStorage.getItem('market') || '[]' )
 const product = JSON.parse(localStorage.getItem('product') || '[]' )
-
+const form = document.querySelector('#addForm')
 myApp.config(function ($routeProvider) {
 
     $routeProvider
@@ -27,6 +27,10 @@ myApp.config(function ($routeProvider) {
             templateUrl: 'pages/location.html',
             controller: 'locationController'
         })
+        .when('/realloD', {
+        templateUrl: 'pages/reallo.html',
+        controller: 'realloController'
+    })
 });
 
 //SERVICES
@@ -39,7 +43,7 @@ myApp.service('cityService', function() {
 
 //CONTROLLERS
 
-myApp.controller('locationController', ['$scope', '$modal', '$log','cityService',, function($scope,$log,$modal,cityService) {
+myApp.controller('locationController', ['$scope', '$modal', '$log','cityService', function($scope,$log,$modal,cityService) {
     $scope.loc = cityService.loc;
 
     $scope.$watch('loc', function() {
@@ -55,7 +59,7 @@ myApp.controller('locationController', ['$scope', '$modal', '$log','cityService'
             phone : shops.phone,
             contactP : shops.contactP,
             module: shops.module,
-            depth : shops.depth
+            depth : shops.depth,
         })
         localStorage.setItem('market', JSON.stringify(market))
         console.log($scope.markets)
@@ -166,6 +170,8 @@ myApp.controller("secondController", ['$scope', '$modal', '$log','$compile',
 
                 product.forEach(function (aProduct) {
                     if (aProduct.eanCode === result.codeResult.code) {
+                        Quagga.stop();
+
                         console.log(aProduct.eanCode + '  and  '+ result.codeResult.code)
                         pname = document.querySelector('#pname')
                         pname.innerHTML = aProduct.pName
@@ -193,6 +199,7 @@ myApp.controller("secondController", ['$scope', '$modal', '$log','$compile',
    
   `
                         $table.appendChild(row)
+
                         console.log($("#pname").text().length)
 
                     }
@@ -255,22 +262,14 @@ myApp.controller("secondController", ['$scope', '$modal', '$log','$compile',
 }]);
 
 
-myApp.controller('productController', ['$scope', '$routeParams', 'cityService', function($scope, $routeParams, cityService) {
-    $scope.city = cityService.city;
-
-}]);
 myApp.controller('dataController', ['$scope', '$routeParams', function($scope, $routeParams) {
     let table = document.querySelector('#product-table')
 
-    let toti = []
-
-    toti = product.filter(function (aproduct) {
-
-        return aproduct.category === "ToTi"
-
-    })
+    let toti = _.where(product, {category: "ToTi"});
     $scope.totis = toti
     console.log(toti)
+
+
 
 
     toti.forEach(function (atoti) {
@@ -294,12 +293,116 @@ myApp.controller('dataController', ['$scope', '$routeParams', function($scope, $
       `
 
         table.appendChild(row)
+    })
+    table.addEventListener('click', function (event) {
+
+        event.preventDefault()
+        let updateButton = event.target
+        let row = updateButton.closest('tr')
+        let id = row.dataset.id
+        let action = updateButton.dataset.action
+
+        if (action === 'update'){
+            let input = row.querySelectorAll('input')
+            let face = input[0].value
+
+
+            product.forEach(function (aProduct) {
+                if (aProduct.eanCode === id){
+                    aProduct.pface =face
+
+                }
+            })
+
+            localStorage.setItem('product', JSON.stringify(product))
+            console.log(product)
 
 
 
+        }
 
     })
 
 
+
+}])
+myApp.controller('realloController', ['$scope',  function($scope) {
+    const bw = 1024;
+    const bh = 600;
+    const mT = 44
+    const sca =35
+    const pl = 13
+    const m = 8
+
+    const wc = 60
+    const talo = 26
+    const nenä = 15
+
+    const nenaWidth = (nenä/(wc+talo+nenä))*100
+    const taloWidth = (talo/(wc+talo+nenä))*100
+    const wcWidth = (wc/(wc+talo+nenä))*100
+
+    console.log(nenaWidth)
+
+    const partNena = bw*nenaWidth/100
+    const partTalo = bw*taloWidth/100
+    const partWc = bw*wcWidth/100
+
+    console.log(partWc)
+    console.log(partTalo)
+    console.log(partNena)
+
+    const canvas = document.getElementById('myCanvas');
+    const context = canvas.getContext('2d');
+    canvas.width  = bw + 1;
+    canvas.height = bh + 1;
+
+
+    const moduleN = 5
+    const shelveN = 3
+
+    function drawBoard(){
+        for (let x = 0; x <= bw; x += bw/moduleN) {
+            context.moveTo(0.5 + x , 0);
+            context.lineTo(0.5 + x , bh);
+        }
+
+
+        for (let x = 0; x <= bh; x += bh/shelveN) {
+            context.moveTo(0, 0.5 + x );
+            context.lineTo(bw , 0.5 + x);
+        }
+
+        context.strokeStyle = "black";
+        context.stroke();
+    }
+    drawBoard();
+
+
+
+    canvas.addEventListener('click', function (event) {
+
+        const rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+        console.log("x: " + x + " y: " + y);
+
+    })
+
+    context.moveTo(partNena, 0);
+    context.lineTo(partNena, bh);
+    //context.lineWidth = 15;
+    context.stroke();
+
+
+    context.fillStyle = "#FF0000";
+    context.fillRect(0, 0, 150, 100);
+    context.fillStyle = "#FFf000";
+    context.fillRect(150, 0, 150, 100);
+
+}])
+
+myApp.controller('productController', ['$scope', '$routeParams', 'cityService', function($scope, $routeParams, cityService) {
+    $scope.city = cityService.city;
 
 }]);
